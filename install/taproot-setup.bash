66ea935de5c79781eb51d38ba91e14ac831fa00d
Mitchell D Scott <50934673+Mitchelldscott@users.noreply.github.com>
2021-09-27 11:35:05 -0600

    fixed some bugs

--- install/taproot-setup.bash
+++ install/taproot-setup.bash
@@ -12,16 +12,16 @@ pip3 install -r python3_requirements.txt
 
 echo -e "\n\tExtracting gcc...\n"
 
-tar -xvf ${HOME}/Downloads/gcc-arm-none-eabi-10-2020-q4-update-x86_64-linux.tar.bz2 -C ~/
-
-echo -e "\n\tSetting your PATH...\n"
-export PATH=${HOME}/gcc-arm-none-eabi-10-2020-q4-major/bin:${HOME}/.local/bin:$PATH
+tar -xvf ${HOME}/Downloads/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2 -C ~/
 
 echo -e "\n\tStaching Cheats...\n"
 echo "export PATH=${PATH}" >> ${HOME}/.bashrc
 
+echo -e "\n\tSetting your PATH...\n"
+export PATH=${HOME}/gcc-arm-none-eabi-10-2020-q4-major/bin:${HOME}/.local/bin:${PATH}
+
 source /etc/os-release
-if [[ ${VERSION}=="18.04.5 LTS (Bionic Beaver)" ]]; then
+if [[ "${VERSION}" == "18.04.5 LTS (Bionic Beaver)" ]]; then
 
 	echo -e "\n\t18.04.5 LTS (Bionic Beaver) Detected adding alias\n"
 	echo 'alias scons="/usr/bin/env python3 $(which scons)"' >> ${HOME}/.bashrc
@@ -52,8 +52,8 @@ cd ${DIR}/..
 echo -e "\n\tInitializing submodules...\n"
 git submodule update --init --recursive
 
-git remote add template https://gitlab.com/aruw/controls/taproot-template-project.git
-git pull template develop
-git push --set-upstream origin master
+# git remote add template https://gitlab.com/aruw/controls/taproot-template-project.git
+# git pull template develop
+# git push --set-upstream origin master
 echo -e "\nSetup Finished!"
 
