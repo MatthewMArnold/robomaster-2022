b53e131b3921ef9e7c9e981d400d1e2492d11cf2
Mitchell D Scott <50934673+Mitchelldscott@users.noreply.github.com>
2021-09-27 11:38:25 -0600

    syntax fix

--- install/taproot-setup.bash
+++ install/taproot-setup.bash
@@ -15,10 +15,10 @@ echo -e "\n\tExtracting gcc...\n"
 tar -xvf ${HOME}/Downloads/gcc-arm-none-eabi-10-2020-q4-update-x86_64-linux.tar.bz2 -C ~/
 
 echo -e "\n\tStaching Cheats...\n"
-echo "export PATH=${HOME}/gcc-arm-none-eabi-10-2020-q4-major/bin:${HOME}/.local/bin:$PATH" >> ${HOME}/.bashrc
+echo "export PATH=${HOME}/gcc-arm-none-eabi-10-2020-q4-major/bin:${HOME}/.local/bin:${PATH}" >> ${HOME}/.bashrc
 
 echo -e "\n\tSetting your PATH...\n"
-export PATH=${HOME}/gcc-arm-none-eabi-10-2020-q4-major/bin:${HOME}/.local/bin:$PATH
+export PATH=${HOME}/gcc-arm-none-eabi-10-2020-q4-major/bin:${HOME}/.local/bin:${PATH}
 
 source /etc/os-release
 if [[ "${VERSION}" == "18.04.5 LTS (Bionic Beaver)" ]]; then
@@ -52,11 +52,9 @@ cd ${DIR}/..
 echo -e "\n\tInitializing submodules...\n"
 git submodule update --init --recursive
 
-<<<<<<< HEAD
-=======
 # git remote add template https://gitlab.com/aruw/controls/taproot-template-project.git
 # git pull template develop
 # git push --set-upstream origin master
->>>>>>> 66ea935de5c79781eb51d38ba91e14ac831fa00d
+
 echo -e "\nSetup Finished!"
 
